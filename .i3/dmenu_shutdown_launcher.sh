f149eb7dc4768c509e977faa688756df9a852322
markus-bauer <mail.markus.bauer@gmail.com>
2016-11-14 18:37:43 +0100

    fixed shutdown_launcher, conkyrc for laptop, added rm to install

--- .i3/dmenu_shutdown_launcher.sh
+++ .i3/dmenu_shutdown_launcher.sh
@@ -11,7 +11,7 @@ TITLE="$(printf "(SLEEP) pm-suspend\n(RB) reboot\n(OFF) poweroff" | ${DMENU})"
 if [ "$TITLE" == "(OFF) poweroff" ]; then
     gnome-terminal -e "bash -c poweroff;bash"
 elif [ "$TITLE" == "(SLEEP) pm-suspend" ]; then
-    gnome-terminal -e "bash -c reboot;bash"
-elif [ "$TITLE" == "(SLEEP) pm-suspend" ]; then
     gnome-terminal -e "sudo pm-suspend;i3lock"
+elif [ "$TITLE" == "(RB) reboot" ]; then
+    gnome-terminal -e "bash -c reboot;bash"
 fi
